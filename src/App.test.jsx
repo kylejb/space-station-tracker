b9a1e1b6861f1eb5728f1cc7634e49007dd6bd57
kylejb <47502769+kylejb@users.noreply.github.com>
2021-02-17 20:41:00 -0500

    Revised test description for clarity

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App Component', () => {
   render(<App />);
   const linkElement = screen.getByText(/App Component/i);
   expect(linkElement).toBeInTheDocument();
