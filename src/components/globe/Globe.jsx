e85786d206b02ce3cc149f993b91a66579c213fe
kylejb <47502769+kylejb@users.noreply.github.com>
2021-02-18 13:21:12 -0500

    Hooked up live ISS data via external API
    Globe renders current location of ISS every five seconds.
    
    Note: stationObj is an array because Globe requires data to be iterable.

--- src/components/globe/Globe.jsx
+++ src/components/globe/Globe.jsx
@@ -3,14 +3,24 @@ import Globe from 'react-globe.gl';
 
 
 const Earth = () => {
-  const [stationLocation, setStationLocation] = useState([]);
+  // react-globe expects stationObj to be iterable
+  const [stationObj, setStationObj] = useState([]);
   const globeEl = useRef();
 
 
+
   useEffect(()=> {
-    //! For illustrative/testing purposes (not for production)
-    setStationLocation([{ coordinates: ["12.25958", "16.03003"] }]);
-  }, [])
+    // ISS Satellite ID is 25544 at this endpoint
+    const findISS = async () => {
+      const response = await fetch("https://api.wheretheiss.at/v1/satellites/25544");
+      let data = await response.json();
+      setStationObj([ data ]);
+    }
+    const interval = setInterval(() => {
+      findISS();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
 
 
   return (
@@ -19,9 +29,9 @@ const Earth = () => {
       <Globe
         ref={globeEl}
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-        labelsData={stationLocation}
-        labelLat={d => d.coordinates[1]}
-        labelLng={d => d.coordinates[0]}
+        labelsData={stationObj}
+        labelLat={d => d.latitude}
+        labelLng={d => d.longitude}
         labelText={d => "ISS"}
         labelSize={1000 * 4e-4}
         labelDotRadius={1400 * 4e-4}
