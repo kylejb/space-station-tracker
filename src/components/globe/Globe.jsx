2358f0b6061a6173b5e838c6fb00fc4c3f5b0734
kylejb <47502769+kylejb@users.noreply.github.com>
2021-02-17 23:48:20 -0500

    Renders with static red circle for ISS [WIP]

--- src/components/globe/Globe.jsx
+++ src/components/globe/Globe.jsx
@@ -1,7 +1,35 @@
-const Globe = () => {
+import { useEffect, useRef, useState } from 'react';
+import Globe from 'react-globe.gl';
+
+
+const Earth = () => {
+  const [stationLocation, setStationLocation] = useState([]);
+  const globeEl = useRef();
+
+
+  useEffect(()=> {
+    //! For illustrative/testing purposes (not for production)
+    setStationLocation([{ name: "ISS", coordinates: ["12.25958", "16.03003"] }]);
+  }, [])
+
+
   return (
-    <h1>Globe Component</h1>
+    <>
+      <h1>Globe Component</h1>
+      <Globe
+        ref={globeEl}
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        labelsData={stationLocation}
+        labelLat={d => d.coordinates[1]}
+        labelLng={d => d.coordinates[0]}
+        labelText={d => d.name}
+        labelSize={1000 * 4e-4}
+        labelDotRadius={1400 * 4e-4}
+        labelColor={() => 'rgba(255, 30, 0, 0.75)'}
+        labelResolution={2}
+      />
+    </>
   );
 };
 
-export default Globe;
+export default Earth;
