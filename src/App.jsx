d5ff9c34fb03481f5a7fc768e9e48346537ae4d7
kylejb <47502769+kylejb@users.noreply.github.com>
2021-03-01 16:11:35 -0500

    Render Search component at App level for dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Globe from "components/globe";
+import Search from "components/search";
 
 const App = () => {
   return (
     <div className="App">
       <h1>App Component</h1>
+      <Search />
       <Globe />
     </div>
   );
