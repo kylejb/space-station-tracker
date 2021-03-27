5b0ab6d5f42f2d923a732fa2a405c6363ee34e3f
kylejb <47502769+kylejb@users.noreply.github.com>
2021-03-27 14:11:41 -0400

    Removed comments and reformatted

--- src/components/sightingtable/Sightingtable.jsx
+++ src/components/sightingtable/Sightingtable.jsx
@@ -1,12 +1,8 @@
-
-
 const SightingTable = ({ tableData }) => {
-
   const chartRows = () => {
     return tableData.map( row => <p>{row}</p> )
   }
-
-    return(
+    return (
       <>
         <h2>Search results below</h2>
         { !tableData
@@ -26,52 +22,3 @@ const SightingTable = ({ tableData }) => {
 
 
 export default SightingTable
-
-/*
-Where you got stuck trying to convert stuff to state - forever re-rendering error:
-
-const [latitude, setLatitude] = useState("")
-    const [longitude, setLongitude] = useState("")
-    const [country, setCountry] = useState("")
-    const [state, setState] = useState("")
-    const [cityList, setCityList] = useState("")
-    const [cityName, setCityName] = useState("")
-
-    useEffect((tableData) => {
-        if(tableData)
-            {
-                setLatitude( tableData.lat )
-                setLongitude( tableData.lon )
-                setCountry( tableData.display_name.split(", ")[4].replace(" ","_") )
-                setState( tableData.display_name.split(", ")[2].replace(" ","_") )
-            }
-        }, [tableData]
-    )
-
-    const getCityArray = () => {
-        if(country && state){
-            const geoLibCityArray = geoMap[country][state]
-            setCityList(geoMap[country][state])
-
-            // cityList = geoMap[country][state].map((cityObj) => {
-            //     return {...cityObj}
-            // })
-
-            geoLibCityArray.forEach((cityObj) => {
-                delete cityObj['city']
-            })
-            return geoLibCityArray
-        }
-    }
-    console.log(latitude)
-
-    //Will probably want to refactor to render chart conditionally - using a useEffect?
-    if(country && state){
-        const closestLatLon = findNearest({latitude: latitude,longitude: longitude} ,getCityArray())
-        // cityName = cityList.find((city) => city["latitude"] === closestLatLon.latitude && city["longitude"] === closestLatLon.longitude)
-        // console.log(cityName)
-    }
-
-
-
-*/
