146e15011d54cbcd4aa1c41442308c4c86f0cc86
kylejb <47502769+kylejb@users.noreply.github.com>
2021-04-01 16:12:38 -0400

    Changed to render sightingData as table

--- src/components/sightingtable/SightingTable.jsx
+++ src/components/sightingtable/SightingTable.jsx
@@ -1,23 +1,38 @@
 const SightingTable = ({ tableData }) => {
   const chartRows = () => {
-    return tableData.map( row => <p>{row}</p> )
+    let count = -1
+    return tableData.map( rowObj => <tr key={++count}>
+      <td>{rowObj.date}</td>
+      <td>{rowObj.time}</td>
+      <td>{rowObj.duration}</td>
+      <td>{rowObj.maxElevation}</td>
+      <td>{rowObj.approach}</td>
+      <td>{rowObj.departure}</td>
+    </tr> )
   }
-    return (
-      <>
-        <h2>Search results below</h2>
-        { !tableData
-          ?
-            <p>No results yet, please search above</p>
-          :
-            <p>
-              Chart data!!! ...after you search and then hit fetch button
-              {chartRows()}
-              Also example of needing to conditionally render with useState
-            </p>
-        }
-      </>
-    )
-
+  return (
+    <>
+      <h1>SightingTable Component</h1>
+      { !tableData
+        ?
+          <p>No results yet, please search above</p>
+        :
+          <table>
+            <tbody>
+              <tr>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Duration</th>
+                <th>Maximum Elevation</th>
+                <th>Approach</th>
+                <th>Departure</th>
+              </tr>
+                {chartRows()}
+            </tbody>
+          </table>
+      }
+    </>
+  );
 }
 
 export default SightingTable;
