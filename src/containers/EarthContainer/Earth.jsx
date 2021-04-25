37832c3768a881be0d406f99586714841df4bd36
kylejb <47502769+kylejb@users.noreply.github.com>
2021-04-25 17:40:48 -0400

    Prototyping new Globe with NASA's worldwindjs lib

--- src/containers/EarthContainer/Earth.jsx
+++ src/containers/EarthContainer/Earth.jsx
@@ -1,14 +1,11 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
+import EarthCanvas from 'components/earth';
 import useViewport from 'hooks/useViewport';
-import * as THREE from 'three';
-import Globe from 'react-globe.gl';
 import './style.scss';
 
 
 const Earth = ( props ) => {
-  const globeEl = useRef();
   const [satelliteCollection, setSatelliteCollection] = useState([]);
-  const [followISS, setFollowISS] = useState(false);
 
 
   useEffect(()=> {
@@ -24,46 +21,7 @@ const Earth = ( props ) => {
     return () => clearInterval(interval);
   }, []);
 
-
-  // Camera follows ISS on state change
-  useEffect(() => {
-    if ( followISS && satelliteCollection.length ) {
-      globeEl.current.controls().autoRotate = false;
-
-      globeEl.current.pointOfView({
-        lat: satelliteCollection[0].latitude,
-        lng: satelliteCollection[0].longitude,
-        altitude: 2
-      });
-    }
-  }, [followISS, satelliteCollection]);
-
-  // Default view
-   useEffect(() => {
-    globeEl.current.pointOfView({
-      lat: 39.9,
-      lng: -97.8,
-      altitude: 2
-    });
-    globeEl.current.controls().autoRotate = true;
-    globeEl.current.controls().autoRotateSpeed = 0.1;
-  }, []);
-
-  useEffect(() => {
-    if ( props.searchResult.length ) {
-      setFollowISS(false);
-      globeEl.current.controls().autoRotate = false;
-      globeEl.current.pointOfView({
-        lat: props.searchResult[0].lat,
-        lng: props.searchResult[0].lon,
-        altitude: 2
-      });
-    }
-
-  }, [props.searchResult]);
-
   const { width, height } = useViewport();
-
 
   return (
     <div className="earth-container">
@@ -75,31 +33,10 @@ const Earth = ( props ) => {
         checked={followISS}
         onChange={() => setFollowISS(!followISS)}
       /> */}
-      <Globe
-        ref={globeEl}
+      <EarthCanvas
         width={width}
         height={height}
-
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-        backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-
-        customLayerData={satelliteCollection}
-        customThreeObject={d => new THREE.Mesh(
-            new THREE.SphereBufferGeometry(4000 * 4e-4),
-            new THREE.MeshLambertMaterial({ color: "white" })
-        )}
-        customThreeObjectUpdate={(obj, d) => {
-            Object.assign(obj.position, globeEl.current.getCoords(d.latitude, d.longitude, 0.4));
-        }}
-
-        labelsData={props.searchResult}
-        labelLat={d => d.lat}
-        labelLng={d => d.lon}
-        labelText={d => "Postcode"}
-        labelSize={1000 * 4e-4}
-        labelDotRadius={1000 * 4e-4}
-        labelColor={() => 'teal'}
-        labelResolution={2}
+        satelliteCollection={satelliteCollection}
       />
     </div>
   );
