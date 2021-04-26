1f13e404c2f6f3ce17f292922db07abf842b4bb6
kylejb <47502769+kylejb@users.noreply.github.com>
2021-04-26 13:15:22 -0400

    Rewired original functionality to globe

--- src/containers/EarthContainer/Earth.jsx
+++ src/containers/EarthContainer/Earth.jsx
@@ -4,8 +4,6 @@ import * as solar from 'solar-calculator';
 import * as THREE from 'three';
 import Globe from 'react-globe.gl';
 import './style.scss';
-import { MeshLambertMaterial } from 'three';
-
 
 const Earth = ( props ) => {
   const globeEl = useRef();
@@ -13,82 +11,81 @@ const Earth = ( props ) => {
   const [followISS, setFollowISS] = useState(false);
 
 
-//   useEffect(()=> {
-//     setTimeout(() => { // wait for scene to be populated (asynchronously)
-//         const directionalLight = globeEl.current.scene().children.find(obj3d => obj3d.type === 'DirectionalLight');
-//         console.log("DL", directionalLight);
-//         directionalLight && directionalLight.position.set(0, 1, 0); // change light position to see the specularMap's effect
-//       });
+  useEffect(()=> {
+    setTimeout(() => { // wait for scene to be populated (asynchronously)
+        const directionalLight = globeEl.current.scene().children.find(obj3d => obj3d.type === 'DirectionalLight');
+        console.log("DL", directionalLight);
+        directionalLight && directionalLight.position.set(0, 1, 0); // change light position to see the specularMap's effect
+      });
 
-//     // ISS Satellite ID is 25544 at this endpoint
-//     // const findISS = async () => {
-//     //   const response = await fetch("https://api.wheretheiss.at/v1/satellites/25544");
-//     //   let data = await response.json();
-//     //   setSatelliteCollection([ data ]);
-//     // }
-//     // const interval = setInterval(() => {
-//     //   findISS();
-//     // }, 5000);
-//     // return () => clearInterval(interval);
-//   }, []);
+    // ISS Satellite ID is 25544 at this endpoint
+    const findISS = async () => {
+      const response = await fetch("https://api.wheretheiss.at/v1/satellites/25544");
+      let data = await response.json();
+      setSatelliteCollection([ data ]);
+    }
+    const interval = setInterval(() => {
+      findISS();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
 
 
-//   // Camera follows ISS on state change
-//   useEffect(() => {
-//     if ( followISS && satelliteCollection.length ) {
-//       globeEl.current.controls().autoRotate = false;
+  // Camera follows ISS on state change
+  useEffect(() => {
+    if ( followISS && satelliteCollection.length ) {
+      globeEl.current.controls().autoRotate = false;
 
-//       globeEl.current.pointOfView({
-//         lat: satelliteCollection[0].latitude,
-//         lng: satelliteCollection[0].longitude,
-//         altitude: 2
-//       });
-//     }
-//   }, [followISS, satelliteCollection]);
+      globeEl.current.pointOfView({
+        lat: satelliteCollection[0].latitude,
+        lng: satelliteCollection[0].longitude,
+        altitude: 2
+      });
+    }
+  }, [followISS, satelliteCollection]);
 
-//   // Default view
-//    useEffect(() => {
-//     globeEl.current.pointOfView({
-//       lat: 39.9,
-//       lng: -97.8,
-//       altitude: 2
-//     });
-//     globeEl.current.controls().autoRotate = true;
-//     globeEl.current.controls().autoRotateSpeed = 0.1;
-//   }, []);
+  // Default view
+   useEffect(() => {
+    globeEl.current.pointOfView({
+      lat: 39.9,
+      lng: -97.8,
+      altitude: 2
+    });
+    globeEl.current.controls().autoRotate = true;
+    globeEl.current.controls().autoRotateSpeed = 0.1;
+  }, []);
 
-//   useEffect(() => {
-//     if ( props.searchResult.length ) {
-//       setFollowISS(false);
-//       globeEl.current.controls().autoRotate = false;
-//       globeEl.current.pointOfView({
-//         lat: props.searchResult[0].lat,
-//         lng: props.searchResult[0].lon,
-//         altitude: 2
-//       });
-//     }
+  useEffect(() => {
+    if ( props.searchResult.length ) {
+      setFollowISS(false);
+      globeEl.current.controls().autoRotate = false;
+      globeEl.current.pointOfView({
+        lat: props.searchResult[0].lat,
+        lng: props.searchResult[0].lon,
+        altitude: 2
+      });
+    }
 
-//   }, [props.searchResult]);
+  }, [props.searchResult]);
 
-    const globeMaterial = new THREE.MeshPhongMaterial();
-    globeMaterial.bumpScale = 10;
-    new THREE.TextureLoader().load(
-        '//unpkg.com/three-globe/example/img/earth-water.png',
-        texture => {
-            globeMaterial.specularMap = texture;
-            globeMaterial.specular = new THREE.Color('grey');
-            globeMaterial.shininess = 15;
-        }
-    );
+    // const globeMaterial = new THREE.MeshPhongMaterial();
+    // globeMaterial.bumpScale = 10;
+    // new THREE.TextureLoader().load(
+    //     '//unpkg.com/three-globe/example/img/earth-water.png',
+    //     texture => {
+    //         globeMaterial.specularMap = texture;
+    //         globeMaterial.specular = new THREE.Color('grey');
+    //         globeMaterial.shininess = 15;
+    //     }
+    // );
     const [dt, setDt] = useState(+new Date());
 
     const VELOCITY = 0; // minutes per frame
 
+    const textureLoader = new THREE.TextureLoader();
     const canvasReDrawTest = () => {
-        const textureLoader = new THREE.TextureLoader;
-
         function texture() {
-            const myMaterial = new MeshLambertMaterial();
+            const myMaterial = new THREE.MeshLambertMaterial();
             myMaterial.onBeforeCompile = shader => {
                 // console.log("vert", shader.vertexShader);
                 // console.log("frag", shader.fragmentShader);
@@ -108,7 +105,13 @@ const Earth = ( props ) => {
     }
 
     const solarMaterial = () => {
-        return new THREE.MeshLambertMaterial({ color: '#414040', opacity: 0.3, transparent: true });
+        return new THREE.MeshLambertMaterial({
+            color: '#2e2e29',
+            clearcoat: 1.0,
+            opacity: 0.3,
+            transparent: true,
+            normalScale: new THREE.Vector2( 0.15, 0.15 ),
+        });
     }
     // const solarMaterial = new THREE.MeshLambertMaterial({ color: '#2e2e29', opacity: 0.3, transparent: true });
 
@@ -126,7 +129,6 @@ const Earth = ( props ) => {
             globeEl.current.iterateTime = requestAnimationFrame(iterateTime);
         }
 
-        // globeEl.current.scene().background = "//unpkg.com/three-globe/example/img/earth-day.jpg"
         globeEl.current = requestAnimationFrame(iterateTime);
         return () => cancelAnimationFrame(globeEl.current.iterateTime);
     }, []);
@@ -166,39 +168,28 @@ const Earth = ( props ) => {
             tileMaterial={() => solarMaterial()}
             tilesTransitionDuration={0}
 
-            globeMaterial={globeMaterial}
+            // globeMaterial={globeMaterial}
             globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
             bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
             backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
 
-            // customLayerData={canvasReDrawTest()}
-            // customThreeObject={d => (d
-            //         // const geometry = new THREE.SphereGeometry( 50, 60, 60, Math.PI, Math.PI, 3*Math.PI/2);
-            //         // const material = d;
-            //         // const mesh = new THREE.Mesh( geometry, material );
-            //         // mesh.material.side = THREE.DoubleSide;
-            //     )}
-            // customThreeObjectUpdate={(obj, d) => {
-            //     Object.assign(obj.position, globeEl.current.getCoords(d.latitude, d.longitude, 0.4));
-            // }}
+            customLayerData={satelliteCollection}
+            customThreeObject={d => new THREE.Mesh(
+                new THREE.SphereBufferGeometry(4000 * 4e-4),
+                new THREE.MeshLambertMaterial({ color: "white" }),
+            )}
+            customThreeObjectUpdate={(obj, d) => {
+                Object.assign(obj.position, globeEl.current.getCoords(d.latitude, d.longitude, 0.4));
+            }}
 
-            // customLayerData={satelliteCollection}
-            // customThreeObject={d => new THREE.Mesh(
-            //     new THREE.SphereBufferGeometry(4000 * 4e-4),
-            //     new THREE.MeshLambertMaterial({ color: "white" }),
-            // )}
-            // customThreeObjectUpdate={(obj, d) => {
-            //     Object.assign(obj.position, globeEl.current.getCoords(d.latitude, d.longitude, 0.4));
-            // }}
-
-            // labelsData={props.searchResult}
-            // labelLat={d => d.lat}
-            // labelLng={d => d.lon}
-            // labelText={d => "Postcode"}
-            // labelSize={1000 * 4e-4}
-            // labelDotRadius={1000 * 4e-4}
-            // labelColor={() => 'teal'}
-            // labelResolution={2}
+            labelsData={props.searchResult}
+            labelLat={d => d.lat}
+            labelLng={d => d.lon}
+            labelText={d => "Postcode"}
+            labelSize={1000 * 4e-4}
+            labelDotRadius={1000 * 4e-4}
+            labelColor={() => 'teal'}
+            labelResolution={2}
         />
         </div>
     );
