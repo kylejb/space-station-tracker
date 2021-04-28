6b3f4a1f7fa9e32c896bd19d31a71a2c06e70087
kylejb <47502769+kylejb@users.noreply.github.com>
2021-04-28 16:55:20 -0400

    Completed core MVP enhancements

--- src/containers/SearchContainer/Dropdown/DropdownContainer.jsx
+++ src/containers/SearchContainer/Dropdown/DropdownContainer.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Select from 'react-select';
 import { flag } from 'country-emoji';
 import { countryOptions } from 'data/countryOptions';
@@ -9,56 +9,84 @@ const DropdownContainer = ({ currentUser, setCurrentUser }) => {
   const [userInput, setUserInput] = useState("");
   const [isCountryDropdownClicked, setIsCountryDropdownClicked] = useState(null);
   const [emojiValue, setEmojiValue] = useState("🇺🇸  ▼");
-
+  const selectRef = useRef(null);
 
   const dropdownSelectHelper = (e) => {
     setUserInput(e.value)
-    let emoji = flag(e.value.replace("_", " "))
+    const emoji = flag(e.value.replace("_", " "))
     setEmojiValue(`${emoji}  ▼`)
   }
 
-  const emojiClickHandler = () => {
-    setIsCountryDropdownClicked(!isCountryDropdownClicked)
+  // setTimeout(() => selectRef.current.focus(), 1000);
+  // selectRef.current should be accessed after state is changed
+  const emojiClickHandler = async () => {
+    //! await seems to have the desired effect despite object not being a Promise
+    await setIsCountryDropdownClicked(!isCountryDropdownClicked);
+    selectRef.current.focus();
   }
 
-  const dropDownChangeHandler = () => {
-    setIsCountryDropdownClicked(!isCountryDropdownClicked)
+  /**
+   * Parameters:
+   * SyntheticKeyboardEvent
+   */
+  const keyDownHandler = (e) => {
+    switch (e.key) {
+        case "Escape":
+            // hardcoded to guarantee exit
+            setIsCountryDropdownClicked(false);
+            break;
+        default:
+            break;
+    }
   }
 
   useEffect(() => {
     setCurrentUser({country: userInput.replace(" ", "_")});
-    dropDownChangeHandler();
+    setIsCountryDropdownClicked(false);
   }, [userInput, setCurrentUser]);
 
   const customStyles = {
+    container: (provided, state) => ({
+        ...provided,
+        display: isCountryDropdownClicked ? "block" : "none",
+        focus: true,
+    }),
     control: (provided, state) => ({
       ...provided,
       width: 180,
       height: 25,
     }),
-    menu:(provided, state) => ({
+    menu: (provided, state) => ({
       ...provided,
       width: 180,
-      minHeight: 25
+      minHeight: 25,
     }),
-  }
+}
 
-  return (
-    <div className="dropdown-container">
-      <div className="select-wrapper">
-        <Select
-          value={countryOptions.find(country => (
-            (country.value === userInput) || (country.value === currentUser.country)
-          ))}
-          options={countryOptions}
-          onChange={(e) => dropdownSelectHelper(e)}
-          placeholder="Country"
-          styles={customStyles}
-        />
-      </div>
-    <input type="button" value={emojiValue} id="emojidropdown" onClick={emojiClickHandler}></input>
-    </div>
-  );
+    return (
+        <div className="dropdown-container">
+            <Select
+                ref={selectRef}
+                options={countryOptions}
+                styles={customStyles}
+                placeholder="Country"
+                openMenuOnFocus
+                onChange={dropdownSelectHelper}
+                onKeyDown={keyDownHandler}
+                value={countryOptions.find(country => (
+                    (country.value === userInput) || (country.value === currentUser.country)
+                ))}
+            />
+
+            <input
+                id="emojidropdown"
+                style={{display: isCountryDropdownClicked ? "none" : "block"}}
+                type="button"
+                value={emojiValue}
+                onClick={emojiClickHandler}
+            />
+        </div>
+    );
 }
 
 export default DropdownContainer;
