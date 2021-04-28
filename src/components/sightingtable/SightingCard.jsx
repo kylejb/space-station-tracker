5692a6fba3584b93e97a3192f7e810091ef5dc77
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2021-04-28 13:04:21 -0400

    sighting card details rendering onlick

--- src/components/sightingtable/SightingCard.jsx
+++ src/components/sightingtable/SightingCard.jsx
@@ -1,17 +1,34 @@
 import './style.scss'
+import {useState} from 'react'
 
 const SightingCard = ({ sightingData }) => {
-      
+const [renderDetails, setrenderDetails] = useState(false)
+
+const renderDetailsHelper = () => {
+  setrenderDetails(!renderDetails)
+}
+
     return ( 
         
-        <div className="sightingcard">
-            <span>{sightingData.date.toDateString()}</span>
-            <span>{sightingData.time}</span>
-            <span>{sightingData.duration}</span>
-            <span>{sightingData.maxElevation}°</span>
-            <span>{sightingData.approach}</span>
-            <span>{sightingData.departure}</span>
-        </div> )
+        <div className="sightingcard" onClick={renderDetailsHelper}>
+              <div className="card_overview">
+                <span>{sightingData.date.toDateString()}</span>
+                <span>{sightingData.time}</span>
+                <span>{sightingData.duration}</span>
+              </div>
+              {renderDetails 
+                ? 
+              <div className="card_detail">
+                <span>{sightingData.maxElevation}°</span>
+                <span>{sightingData.approach}</span>
+                <span>{sightingData.departure}</span>
+              </div>
+                :
+              null
+    
+              }
+        </div> 
+        )
 }
 
 export default SightingCard
