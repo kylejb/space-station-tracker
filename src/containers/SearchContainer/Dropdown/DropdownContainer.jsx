d86d769b737766603e0591ce9c92fbcd6179acd7
kylejb <47502769+kylejb@users.noreply.github.com>
2021-04-28 16:57:06 -0400

    Reformatted file: indentation set to 4 spaces

--- src/containers/SearchContainer/Dropdown/DropdownContainer.jsx
+++ src/containers/SearchContainer/Dropdown/DropdownContainer.jsx
@@ -6,62 +6,62 @@ import './style.scss';
 
 
 const DropdownContainer = ({ currentUser, setCurrentUser }) => {
-  const [userInput, setUserInput] = useState("");
-  const [isCountryDropdownClicked, setIsCountryDropdownClicked] = useState(null);
-  const [emojiValue, setEmojiValue] = useState("🇺🇸  ▼");
-  const selectRef = useRef(null);
+    const [userInput, setUserInput] = useState("");
+    const [isCountryDropdownClicked, setIsCountryDropdownClicked] = useState(null);
+    const [emojiValue, setEmojiValue] = useState("🇺🇸  ▼");
+    const selectRef = useRef(null);
 
-  const dropdownSelectHelper = (e) => {
-    setUserInput(e.value)
-    const emoji = flag(e.value.replace("_", " "))
-    setEmojiValue(`${emoji}  ▼`)
-  }
-
-  // setTimeout(() => selectRef.current.focus(), 1000);
-  // selectRef.current should be accessed after state is changed
-  const emojiClickHandler = async () => {
-    //! await seems to have the desired effect despite object not being a Promise
-    await setIsCountryDropdownClicked(!isCountryDropdownClicked);
-    selectRef.current.focus();
-  }
-
-  /**
-   * Parameters:
-   * SyntheticKeyboardEvent
-   */
-  const keyDownHandler = (e) => {
-    switch (e.key) {
-        case "Escape":
-            // hardcoded to guarantee exit
-            setIsCountryDropdownClicked(false);
-            break;
-        default:
-            break;
+    const dropdownSelectHelper = (e) => {
+        setUserInput(e.value)
+        const emoji = flag(e.value.replace("_", " "))
+        setEmojiValue(`${emoji}  ▼`)
     }
-  }
 
-  useEffect(() => {
-    setCurrentUser({country: userInput.replace(" ", "_")});
-    setIsCountryDropdownClicked(false);
-  }, [userInput, setCurrentUser]);
+    // setTimeout(() => selectRef.current.focus(), 1000);
+    // selectRef.current should be accessed after state is changed
+    const emojiClickHandler = async () => {
+        //! await seems to have the desired effect despite object not being a Promise
+        await setIsCountryDropdownClicked(!isCountryDropdownClicked);
+        selectRef.current.focus();
+    }
 
-  const customStyles = {
-    container: (provided, state) => ({
-        ...provided,
-        display: isCountryDropdownClicked ? "block" : "none",
-        focus: true,
-    }),
-    control: (provided, state) => ({
-      ...provided,
-      width: 180,
-      height: 25,
-    }),
-    menu: (provided, state) => ({
-      ...provided,
-      width: 180,
-      minHeight: 25,
-    }),
-}
+    /**
+     * Parameters:
+     * SyntheticKeyboardEvent
+     */
+    const keyDownHandler = (e) => {
+        switch (e.key) {
+            case "Escape":
+                // hardcoded to guarantee exit
+                setIsCountryDropdownClicked(false);
+                break;
+            default:
+                break;
+        }
+    }
+
+    useEffect(() => {
+        setCurrentUser({country: userInput.replace(" ", "_")});
+        setIsCountryDropdownClicked(false);
+    }, [userInput, setCurrentUser]);
+
+    const customStyles = {
+        container: (provided, state) => ({
+            ...provided,
+            display: isCountryDropdownClicked ? "block" : "none",
+            focus: true,
+        }),
+        control: (provided, state) => ({
+            ...provided,
+            width: 180,
+            height: 25,
+        }),
+        menu: (provided, state) => ({
+            ...provided,
+            width: 180,
+            minHeight: 25,
+        }),
+    }
 
     return (
         <div className="dropdown-container">
