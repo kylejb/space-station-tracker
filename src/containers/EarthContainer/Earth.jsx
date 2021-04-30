837e7945c7ef9c8224c5b645536e37a2395bb809
kylejb <47502769+kylejb@users.noreply.github.com>
2021-04-29 23:37:06 -0400

    Refactored to support UI-centric file struc [WIP]

--- src/containers/EarthContainer/Earth.jsx
+++ src/containers/EarthContainer/Earth.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
-import EarthCanvas from 'components/earth';
-import useViewport from 'hooks/useViewport';
+import Globe from 'api/globe/Globe';
+import WorldWind from 'worldwindjs';
 import './style.scss';
 
-const Earth = ( props ) => {
+const EarthContainer = ( props ) => {
   const [satelliteCollection, setSatelliteCollection] = useState([]);
 
 
@@ -20,28 +20,41 @@ const Earth = ( props ) => {
     return () => clearInterval(interval);
   }, []);
 
-  const { width, height } = useViewport();
+    //! Experimental - for testing/development of Canvas component and api/Globe
+    // useEffect(() => {
+    //     // Create a globe
+    //     let globe = new Globe("globe-canvas");
+
+    //     // Add layers to the globe
+    //     globe.addLayer(new WorldWind.BMNGLayer(), {
+    //         category: "base"
+    //     });
+    //     globe.addLayer(new WorldWind.CoordinatesDisplayLayer(globe.wwd), {
+    //         category: "setting"
+    //     });
+    //     globe.addLayer(new WorldWind.ViewControlsLayer(globe.wwd), {
+    //         category: "setting"
+    //     });
+    //     globe.addLayer(new WorldWind.CompassLayer(), {
+    //         category: "setting",
+    //     });
+    // },[]);
 
     return (
         <div className="earth-container">
             <h1>Where iss the ISS?</h1>
-                <span>
-                    <input
-                        aria-label="Toggle to follow ISS"
-                        type="checkbox"
-                        // value={followISS}
-                        // checked={followISS}
-                        // onChange={() => setFollowISS(!followISS)}
-                    />
-                <label aria-label="Follow ISS">Follow Station</label>
-                </span>
-            <EarthCanvas
-                width={width}
-                height={height}
-                satelliteCollection={satelliteCollection}
-            />
+            <span>
+                <input
+                    aria-label="Toggle to follow ISS"
+                    type="checkbox"
+                    // value={followISS}
+                    // checked={followISS}
+                    // onChange={() => setFollowISS(!followISS)}
+                />
+            <label aria-label="Follow ISS">Follow Station</label>
+            </span>
         </div>
     );
 };
 
-export default Earth;
+export default EarthContainer;
