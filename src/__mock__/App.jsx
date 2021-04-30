e686ed1bdf314882b5c773b2ba12eb00aff15a7d
kylejb <47502769+kylejb@users.noreply.github.com>
2021-04-30 03:47:30 -0400

    Removed stale files

--- src/__mock__/App.jsx
+++
@@ -1,8 +0,0 @@
-const MockApp = () => <>
-  <h1>App Component</h1>
-  <h1>Search Component</h1>
-  <h1>Globe Component</h1>
-</>;
-
-MockApp.create = jest.fn(() => MockApp);
-export default MockApp;
