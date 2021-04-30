7506f3f8588c5e11aae05e0eed3b1aa0006633c2
kylejb <47502769+kylejb@users.noreply.github.com>
2021-04-30 17:03:48 -0400

    Added forwardRef to accept Ref from Earth

--- src/common/components/canvas/Canvas.jsx
+++ src/common/components/canvas/Canvas.jsx
@@ -1,18 +1,20 @@
+import { forwardRef } from 'react';
 import useViewport from 'common/hooks/useViewport';
 import './style.scss';
 
 
-const Canvas = ({id, ...props}) => {
+const Canvas = forwardRef(({ id, ...props }, ref) => {
     const {width, height} = useViewport();
 
     return (
         <div className="canvas-wrapper" width={width} height={height} style={{ position: "fixed" }}>
             <canvas
                 id={id}
+                ref={ref}
                 style={{ width: "100%", height: "100%" }}
             >Your browser does not support HTML5 Canvas.</canvas>
         </div>
     );
-}
+});
 
 export default Canvas;
