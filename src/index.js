3e9df88276d856d6b98051dc6ac83b1506625ad4
kylejb <47502769+kylejb@users.noreply.github.com>
2021-04-30 00:56:15 -0400

    Refactored App's entry point to AppContainer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from 'containers/AppContainer';
 import 'styles/base.scss';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
