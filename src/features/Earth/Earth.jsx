cad047ed821ac50625ee4adceffc86dba2212cf4
kylejb <47502769+kylejb@users.noreply.github.com>
2021-04-30 20:01:53 -0400

    Added starry background and satellite on (lat,lng)

--- src/features/Earth/Earth.jsx
+++ src/features/Earth/Earth.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import Canvas from 'common/components/canvas';
-import Globe from 'common/components/globe';
+import Globe from './globe';
 
 import EoxSentinal2CloudlessLayer from 'common/api/EoxSentinal2CloudlessLayer';
 import EnhancedAtmosphereLayer from 'common/api/EnhancedAtmosphereLayer';
+import EnhancedStarFieldLayer from 'common/api/EnhancedStarFieldLayer';
 
 import WorldWind from 'worldwindjs';
 
@@ -28,13 +29,110 @@ const Earth = ({
     const setBaseUrl = (baseUrl) => {
         WorldWind.configuration.baseUrl = baseUrl;
     }
+
+    const renderSatellite = () => {
+        const { wwd } = earthMetadata;
+        const modelLayer = new WorldWind.RenderableLayer();
+        wwd.addLayer(modelLayer);
+
+        // Position takes the following position arguments: lat, lon, altitude
+        const position = new WorldWind.Position(-51.151321133767, -6.0196159367604, 800000.0);
+        const config = {dirPath: WorldWind.configuration.baseUrl};
+        const colladaLoader = new WorldWind.ColladaLoader(position, config);
+
+        colladaLoader.load('satellite.dae', function (colladaModel) {
+            colladaModel.scale = 1500;
+            modelLayer.addRenderable(colladaModel);
+        })
+    }
     useEffect(() => {
-        const globe = new Globe("globe-canvas");
-        globe.addLayer(new WorldWind.BMNGLayer(), {
-        // Add layers to the globe
+        const wwd = new Globe("globe-canvas");
+
+        wwd.addLayer(new WorldWind.BMNGLayer(), {
+            // Add layers to the globe
             category: "base"
         });
+
+        // wwd.addLayer(new WorldWind.StarFieldLayer(), {
+        //     category: "star-background"
+        // });
+
+        setEarthMetadata(prevState =>({
+            ...prevState,
+            wwd: wwd.wwd,
+        }));
     }, []);
+
+    useEffect(() => {
+        const { wwd } = earthMetadata;
+
+        // Tell WorldWind to log only warnings and errors.
+        WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
+
+        if (wwd) {
+
+            //* needs useCallback, if implemented this way
+            renderSatellite();
+
+            // Create the WorldWindow.
+            // Create imagery layers.
+            // const BMNGOneImageLayer = new WorldWind.BMNGOneImageLayer("https://github.com/NASAWorldWind/WebWorldWind/blob/develop/images/BMNG_world.topo.bathy.200405.3.2048x1024.jpg");
+            // const BMNGOneImageLayer = new WorldWind.BMNGOneImageLayer(BMNGWorld);
+            // const BMNGLayer = new WorldWind.BMNGLayer();
+            // wwd.addLayer(BMNGOneImageLayer);
+            // wwd.addLayer(BMNGLayer);
+
+            // Use the StarField layer to show stars and the Sun around the globe, and the Atmosphere layer to display
+            // the atmosphere effect and the night side of the Earth.
+            // Note that the StarField layer requires a dark canvas background color.
+            // The StarField layer should be added before the Atmosphere layer.
+            const starFieldLayer = new WorldWind.StarFieldLayer();
+            const atmosphereLayer = new EnhancedAtmosphereLayer("https://unpkg.com/worldwindjs@1.7.0/build/dist/images/dnb_land_ocean_ice_2012.png");
+            wwd.addLayer(starFieldLayer);
+            wwd.addLayer(atmosphereLayer);
+
+            // Set a date property for the StarField and Atmosphere layers to the current date and time.
+            // This enables the Atmosphere layer to show a night side (and dusk/dawn effects in Earth's terminator).
+            // The StarField layer positions its stars according to this date.
+            const now = new Date();
+            // starFieldLayer.time = now;
+            atmosphereLayer.time = now;
+
+            // In this example, each full day/night cycle lasts 8 seconds in real time.
+            const simulatedMillisPerDay = 10000;
+
+            // Begin the simulation at the current time as provided by the browser.
+            const startTimeMillis = Date.now();
+
+            function runSimulation() {
+                // Compute the number of simulated days (or fractions of a day) since the simulation began.
+                const elapsedTimeMillis = Date.now() - startTimeMillis;
+                const simulatedDays = elapsedTimeMillis / simulatedMillisPerDay;
+
+                // Compute a real date in the future given the simulated number of days.
+                const millisPerDay = 24 * 3600 * 1000; // 24 hours/day * 3600 seconds/hour * 1000 milliseconds/second
+                const simulatedMillis = simulatedDays * millisPerDay;
+                const simulatedDate = new Date(startTimeMillis + simulatedMillis);
+
+                // Update the date in both the Starfield and the Atmosphere layers.
+                // starFieldLayer.time = simulatedDate;
+                atmosphereLayer.time = simulatedDate;
+                wwd.redraw(); // Update the WorldWindow scene.
+
+                canvasRef.current.runSim = requestAnimationFrame(runSimulation);
+            }
+
+            console.log('canvasRef', wwd);
+            // Animate the starry sky as well as the globe's day/night cycle.
+            canvasRef.current.runSim = requestAnimationFrame(runSimulation);
+            const refRunSim = canvasRef.current.runSim;
+            return () => cancelAnimationFrame(refRunSim);
+
+            // Create a layer manager for controlling layer visibility.
+            // const layerManager = new LayerManager(wwd);
+        }
+    }, [earthMetadata.wwd]);
+
 
     return <Canvas ref={canvasRef} id="globe-canvas" />;
 }
