201d8156f0d0caf8034de5886072a2c496be5262
kylejb <47502769+kylejb@users.noreply.github.com>
2021-04-30 22:05:14 -0400

    Refactored with redux-toolkit

--- src/app/store.js
+++ src/app/store.js
@@ -1,23 +1,14 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "common/reducers";
+import { configureStore } from '@reduxjs/toolkit';
+import { earthReducer, searchReducer, userReducer } from 'common/reducers';
 
-const middleware = [ thunk ];
 
-// Setup for development only middleware
-// if (process.env.NODE_ENV !== 'production') {
-//     middleware.push();
-// }
-
-// TODO - Wire up Redux DevTools for development env
-// const composeEnhancers = composeWithDevTools({
-//     // actionsBlacklist, actionsCreators, and other other options if needed
-// });
-
-const Store = createStore(
-    rootReducer,
-    applyMiddleware(...middleware),
-);
+const Store = configureStore({
+    reducer: {
+        earth: earthReducer,
+        search: searchReducer,
+        user: userReducer,
+    },
+});
 
 //? Consider trigger dispatch to populate initial state upon store creation
 // Store.dispatch(getUserAndSatelliteInfo)
--- src/common/reducers.js
+++ src/common/reducers.js
@@ -1,11 +1,5 @@
-import { combineReducers } from 'redux';
 import { reducer as searchReducer } from 'features/search';
 import { reducer as userReducer } from 'features/analytics';
 import { earthReducer } from 'features/earth';
 
-
-export default combineReducers({
-    user: userReducer,
-    search: searchReducer,
-    earth: earthReducer,
-});
+export { searchReducer, userReducer, earthReducer };
