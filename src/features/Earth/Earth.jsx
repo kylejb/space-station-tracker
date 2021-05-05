529d3e1b5485000c445c83a5d42b39395fc74e01
kylejb <47502769+kylejb@users.noreply.github.com>
2021-05-05 12:45:46 -0400

    Refactored layers w/ state & added comments for v2

--- src/features/Earth/Earth.jsx
+++ src/features/Earth/Earth.jsx
@@ -13,17 +13,53 @@ import EnhancedStarFieldLayer from 'common/api/EnhancedStarFieldLayer';
 import WorldWind from 'worldwindjs';
 
 
+const baseLayer = new WorldWind.BMNGLayer();
+const starFieldLayer = new WorldWind.StarFieldLayer();
+const atmosphereLayer = new EnhancedAtmosphereLayer("https://unpkg.com/worldwindjs@1.7.0/build/dist/images/dnb_land_ocean_ice_2012.png");
+
+const issLayer = new WorldWind.RenderableLayer("ISS");
+
+const defaultEarthLayers = [
+    baseLayer,
+    starFieldLayer,
+    atmosphereLayer,
+    issLayer,
+]
+
 const Earth = ({
     searchResult,
     ...props
 }) => {
     const canvasRef = useRef(null);
     const [wwd, setWWD] = useState(null);
+    const [earthLayers, setEarthLayers] = useState(defaultEarthLayers);
+    const [firstLoad, setFirstLoad] = useState(true);
     const satelliteData = useSelector(getSatellite);
     const { status, satelliteCollection } = satelliteData;
 
     const setBaseUrl = (baseUrl) => {
         WorldWind.configuration.baseUrl = baseUrl;
+    }
+
+    // Hardcoded to assume only ISS in SatelliteCollection for MVP
+    const getSatellitePosition = () => new WorldWind.Position(satelliteCollection[0].latitude, satelliteCollection[0].longitude, 80000.0);
+
+    const updateSatellite = () => {
+        const issLayer = earthLayers[3];
+
+        const newPosition = getSatellitePosition(); //! hardcoded
+        for ( let i = 0; i < satelliteCollection.length; i++ ) {
+            issLayer.renderables[i].position.latitude = newPosition.latitude;
+            issLayer.renderables[i].position.longitude = newPosition.longitude;
+            issLayer.renderables[i].position.altitude = newPosition.altitude;
+        }
+
+        /*
+          TODO: update Satellite model's position on state change
+            - issLayer.renderables[i].position is correctly being reset based on above
+            - model does not re-render; is there an alternative to removing and adding new?
+        **
+        */
     }
 
     const renderSatellite = () => {
@@ -33,19 +69,16 @@ const Earth = ({
         // if (wwd.layers[3]) {
         //     new WorldWind.RenderableLayer().removeRenderable(wwd.layers[3].renderables[0])
         // }
-        const modelLayer = new WorldWind.RenderableLayer();
-        wwd.addLayer(modelLayer)
+        const issLayer = earthLayers[3]
+        wwd.addLayer(issLayer)
 
         // Position takes the following position arguments: lat, lon, altitude
-        let position = new WorldWind.Position(satelliteCollection[0].latitude, satelliteCollection[0].longitude, 800000.0);
         const config = {dirPath: WorldWind.configuration.baseUrl};
-        const colladaLoader = new WorldWind.ColladaLoader(position, config);
-
-        //? console.log("satPositionObj", position);
+        const colladaLoader = new WorldWind.ColladaLoader(getSatellitePosition(), config);
 
         colladaLoader.load('satellite.dae', function (colladaModel) {
             colladaModel.scale = 1500;
-            modelLayer.addRenderable(colladaModel);
+            issLayer.addRenderable(colladaModel);
         })
     }
 
@@ -53,47 +86,39 @@ const Earth = ({
 
     useEffect(() => {
         const wwd = new Globe("globe-canvas");
-        wwd.addLayer(new WorldWind.BMNGLayer(), {
+        wwd.addLayer(earthLayers[0], {
             // Add layers to the globe
             category: "base"
         });
 
-        // wwd.addLayer(new WorldWind.StarFieldLayer(), {
-        //     category: "star-background"
-        // });
+        wwd.addLayer(earthLayers[1], {
+            category: "star-background"
+        });
+
+        wwd.addLayer(earthLayers[2], {
+            category: "enhanced-atmosphere"
+        });
 
         setWWD(wwd.wwd);
-        dispatch(fetchISS());
+        const interval = setInterval(() => {
+            dispatch(fetchISS());
+        }, 5000);
+        return () => clearInterval(interval);
     }, []);
+
+    //
 
     useEffect(() => {
         // Tell WorldWind to log only warnings and errors.
         WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
 
         if (wwd) {
-            // Create the WorldWindow.
-            // Create imagery layers.
-            // const BMNGOneImageLayer = new WorldWind.BMNGOneImageLayer("https://github.com/NASAWorldWind/WebWorldWind/blob/develop/images/BMNG_world.topo.bathy.200405.3.2048x1024.jpg");
-            // const BMNGOneImageLayer = new WorldWind.BMNGOneImageLayer(BMNGWorld);
-            // const BMNGLayer = new WorldWind.BMNGLayer();
-            // wwd.addLayer(BMNGOneImageLayer);
-            // wwd.addLayer(BMNGLayer);
-
-            // Use the StarField layer to show stars and the Sun around the globe, and the Atmosphere layer to display
-            // the atmosphere effect and the night side of the Earth.
-            // Note that the StarField layer requires a dark canvas background color.
-            // The StarField layer should be added before the Atmosphere layer.
-            const starFieldLayer = new WorldWind.StarFieldLayer();
-            const atmosphereLayer = new EnhancedAtmosphereLayer("https://unpkg.com/worldwindjs@1.7.0/build/dist/images/dnb_land_ocean_ice_2012.png");
-            wwd.addLayer(starFieldLayer);
-            wwd.addLayer(atmosphereLayer);
-
             // Set a date property for the StarField and Atmosphere layers to the current date and time.
             // This enables the Atmosphere layer to show a night side (and dusk/dawn effects in Earth's terminator).
             // The StarField layer positions its stars according to this date.
             const now = new Date();
-            // starFieldLayer.time = now;
-            atmosphereLayer.time = now;
+            // earthLayers[1].time = now;
+            earthLayers[2].time = now;
 
             // In this example, each full day/night cycle lasts 8 seconds in real time.
             const simulatedMillisPerDay = 10000;
@@ -112,8 +137,8 @@ const Earth = ({
                 const simulatedDate = new Date(startTimeMillis + simulatedMillis);
 
                 // Update the date in both the Starfield and the Atmosphere layers.
-                // starFieldLayer.time = simulatedDate;
-                atmosphereLayer.time = simulatedDate;
+                // earthLayers[1].time = simulatedDate;
+                earthLayers[2].time = simulatedDate;
                 wwd.redraw(); // Update the WorldWindow scene.
 
                 canvasRef.current.runSim = requestAnimationFrame(runSimulation);
@@ -126,11 +151,15 @@ const Earth = ({
             // Create a layer manager for controlling layer visibility.
             // const layerManager = new LayerManager(wwd);
         }
+
     }, [wwd]);
 
     useEffect(() => {
-        if (status === "succeeded") {
+        if (firstLoad && status === "succeeded") {
             renderSatellite();
+            setFirstLoad(false);
+        } else if (status === "succeeded") {
+            updateSatellite();
         }
         // dispatch(fetchISS());
     }, [satelliteCollection]);
