d9e74aa2d744853eab9ad6401f08484f06fc1fd6
kylejb <47502769+kylejb@users.noreply.github.com>
2021-05-13 19:22:09 -0400

    Removed react fragment and minor reformatting

--- src/components/notification/Error.jsx
+++ src/components/notification/Error.jsx
@@ -1,17 +1,15 @@
 import { useErrorContext } from 'common/hooks';
+import './style.scss';
 
-import './style.scss'
 
 const Error = () => {
     const { error } = useErrorContext();
 
     return (
-        <>
-            <div className={`error-${error.type}`}>
-                <h1>{error.message}</h1>
-            </div>
-        </>
-    )
+        <div className={`error-${error.type}`}>
+            <h1>{error.message}</h1>
+        </div>
+    );
 }
 
-export default Error
+export default Error;
