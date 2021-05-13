469ac5f766c1ed654f7bee08a7062a92be82be5c
kylejb <47502769+kylejb@users.noreply.github.com>
2021-05-13 16:31:11 -0400

    Refactored setter and getter

--- src/common/contexts/errors/ErrorProvider.jsx
+++ src/common/contexts/errors/ErrorProvider.jsx
@@ -1,6 +1,7 @@
 import { useState, useCallback } from 'react';
 import ErrorContext from './ErrorContext';
 
+// TODO - refactor logic to flatten error to contain only msg
 const initialState = {
     type: null,
     message: null,
@@ -10,12 +11,18 @@ const initialState = {
 const ErrorProvider = ({ children }) => {
     const [error, setError] = useState(initialState);
 
-    const setErrorHelper = useCallback((arg) => {
-        setError(arg);
-    }, []);
+    const removeError = () => setError({ type: false, message: false });
+
+    const addError = (message, status) => setError({ message, status });
+
+    const contextValue = {
+        error,
+        addError: useCallback((message, status) => addError(message, status), []),
+        removeError: useCallback(() => removeError(), [])
+    };
 
     return (
-        <ErrorContext.Provider value={{error, setErrorHelper}}>
+        <ErrorContext.Provider value={contextValue}>
             {children}
         </ErrorContext.Provider>
     );
