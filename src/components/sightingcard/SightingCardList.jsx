974de46d67255cbfb34f91087dcbda1eb17bfe6a
kylejb <47502769+kylejb@users.noreply.github.com>
2021-05-13 20:39:00 -0400

    Removed data logic to keep component basic

--- src/components/sightingcard/SightingCardList.jsx
+++ src/components/sightingcard/SightingCardList.jsx
@@ -3,34 +3,11 @@ import SightingCard from './SightingCard';
 import { useErrorContext } from 'common/hooks';
 import { SIGHTINGRESULTS_DISTANCE_MESSAGE } from 'utils/constants';
 
-/**
- * Create a historic Date object.
- *
- * Helper function to set a baseline for items to return.
- *
- * @param {number}  numOfDays=1     Set the number of days from present to return with default of 1.
- *
- * @yield {Date}    Returns past date for filtering purposes.
- */
-const filterSightingCardsByDate = (numOfDays=1) => {
-    const dateThreshold = new Date();
-    return new Date(dateThreshold.setDate(dateThreshold.getDate() - numOfDays));
-}
 
 const SightingCardList = ({ tableData }) => {
-
-
-    const filteredSightingCards = () => {
-        return tableData.value?.filter(rowObj => (rowObj.date > filterSightingCardsByDate()
-            && parseInt(rowObj.maxElevation) >= 30
-            && parseInt(rowObj.duration[0])
-        ));
-    }
-
     const renderSightingCards = () => {
-
         let count = -1;
-        return filteredSightingCards()?.map( rowObj => (
+        return tableData.value.map( rowObj => (
             <SightingCard
                 key={++count}
                 sightingData={rowObj}
@@ -47,7 +24,7 @@ const SightingCardList = ({ tableData }) => {
         <>
             <div className="sightingresults">
                 <SightingCard header sightingData={headerData}/>
-                {renderSightingCards()}
+                { tableData.status === "FILTER_SUCCESS" && renderSightingCards()}
             </div>
         </>
     );
