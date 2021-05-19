7439885381d4026408b66b61e5dee394416ba35f
kylejb <47502769+kylejb@users.noreply.github.com>
2021-05-18 20:10:47 -0400

    Add comment to denote code removal for PROD

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type,Accept,X-Access-Token,X-Key');
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*'); //! for DEVELOPMENT - remove before deployment
     next();
 });
 
