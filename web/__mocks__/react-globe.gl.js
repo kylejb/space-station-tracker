ad7ad78a16e28505ba5296220ced239e2dadff2c
Kyle J. Burda <47502769+kylejb@users.noreply.github.com>
2026-01-31 15:07:44 -0500

    chore(web): add mock implementations for three.js and react-globe.gl

---
+++ web/__mocks__/react-globe.gl.js
@@ -0,0 +1,2 @@
+const Globe = () => null;
+export default Globe;
---
+++ web/__mocks__/three.js
@@ -0,0 +1,4 @@
+export const Mesh = jest.fn();
+export const MeshLambertMaterial = jest.fn();
+export const SphereGeometry = jest.fn();
+export const MultiplyOperation = 0;
